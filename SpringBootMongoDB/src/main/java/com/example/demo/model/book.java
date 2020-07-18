0df63a8a1d51a43f93a973df82974c1533d40e50
sandysanthosh <sandysanthosh101@gmail.com>
2020-07-18 13:11:10 +0530

    MongoDB Examples

---
+++ SpringBootMongoDB/src/main/java/com/example/demo/model/book.java
@@ -0,0 +1,37 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Document
+@Data
+
+public class book {
+	@Id
+	private int id;
+	private String bookName;
+	private String authorName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	
+	
+
+}
